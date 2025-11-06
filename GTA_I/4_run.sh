5ae234ce7e18371eac4ea17ec3bb3e5731aa4bc3
publicsite <34146156+publicsite@users.noreply.github.com>
2025-11-06 18:40:48 +0000

    Update 4_run.sh Set umask

--- GTA_I/4_run.sh
+++ GTA_I/4_run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+OLD_UMASK="$(umask)"
+umask 0022
+
 cd "$(dirname "$0")"
 thebin="$(find "$PWD/carnage3d/.build/bin" -type f -name carnage3d | head -n 1)"
 
@@ -6,3 +10,5 @@ if [ -f "$thebin" ]; then
 	cd "$PWD/carnage3d/gamedata"
 	$thebin -gtadata "$PWD" -mapname SANB.CMP
 fi
+
+umask "${OLD_UMASK}"
