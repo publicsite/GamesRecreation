253ad81f37be1ad85e2b0cca813fe491b0040a7f
publicsite <34146156+publicsite@users.noreply.github.com>
2025-11-06 18:39:24 +0000

    Update 1_get.sh Set umask

--- GTA_I/1_get.sh
+++ GTA_I/1_get.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+OLD_UMASK="$(umask)"
+umask 0022
+
 cd "$(dirname "$0")"
 
 #for these scripts
@@ -8,3 +12,5 @@ sudo apt install libglew-dev libglm-dev libgl1-mesa-dev xorg-dev clang
 sudo apt-get install libopenal-dev
 
 git clone --recurse-submodules https://github.com/codenamecpp/carnage3d
+
+umask "${OLD_UMASK}"
