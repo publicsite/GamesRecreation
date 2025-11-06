3620a520f605206475c78b07428285be8c66084e
publicsite <34146156+publicsite@users.noreply.github.com>
2025-11-06 18:45:55 +0000

    Update 1_get.sh Set umask

--- TOMBRAIDER/1_get.sh
+++ TOMBRAIDER/1_get.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+OLD_UMASK="$(umask)"
+umask 0022
+
 cd "$(dirname "$0")"
 
 #for these scripts
@@ -8,3 +11,5 @@ sudo apt-get install wget zenity
 sudo apt-get install libopenal-dev libsdl2-dev libpng12-dev libglu1-mesa-dev zlib1g-dev
 
 wget "https://github.com/opentomb/OpenTomb/archive/refs/heads/master.tar.gz"
+
+umask "${OLD_UMASK}"
