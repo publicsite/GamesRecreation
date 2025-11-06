42a0295e772b18d74a2a5133bb634fa8521dd1ff
publicsite <34146156+publicsite@users.noreply.github.com>
2025-11-06 18:46:27 +0000

    Update 2_extract_assets_tombraider1.sh Set umask

--- TOMBRAIDER/2_extract_assets_tombraider1.sh
+++ TOMBRAIDER/2_extract_assets_tombraider1.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+OLD_UMASK="$(umask)"
+umask 0022
+
 cd "$(dirname "$0")"
 
 mkdir mountpoint
@@ -24,3 +28,5 @@ cp -a mountpoint TombRaider1
 sudo umount mountpoint
 
 rmdir mountpoint
+
+umask "${OLD_UMASK}"
