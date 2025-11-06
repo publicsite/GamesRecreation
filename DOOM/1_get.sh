391c6b1a17a06fe51e7b873ed88abb7345218ed5
publicsite <34146156+publicsite@users.noreply.github.com>
2025-11-06 18:37:16 +0000

    Update 1_get.sh Set umask

--- DOOM/1_get.sh
+++ DOOM/1_get.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+
+OLD_UMASK="$(umask)"
+umask 0022
+
 cd "$(dirname "$0")"
 
 #for these scripts
 sudo apt-get install zenity
 
 sudo apt-get install chocolate-doom
+
+umask "${OLD_UMASK}"
